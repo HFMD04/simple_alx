b57878cc3ee24fbaeb65e028f9e5670780277ed3
SMSB96 <saramiad96@gmail.com>
2023-08-25 09:34:42 -0700

    updates

--- shell.c
+++ shell.c
@@ -13,8 +13,15 @@ size_t buffer_size = MAX_CMD_LEN;
 while (1)
 {
 	char *args[2];
+	pid_t process_id;
 
 	printf("#myshell$ ");
+	user_input = (char *)malloc(buffer_size * sizeof(char));
+	if (user_input == NULL)
+	{
+		perror("Memory allocation error");
+		return (EXIT_FAILURE);
+	}
 	if (getline(&user_input, &buffer_size, stdin) == -1)
 	{
 		if (feof(stdin))
@@ -31,13 +38,28 @@ while (1)
 		}
 	}
 	user_input[strcspn(user_input, "\n")] = '\0';
-	args[0] = user_input;
-	args[1] = NULL;
-	if (execve(user_input, args, environ) == -1)
+
+	process_id = fork();
+	if (process_id == -1)
 	{
-		perror("Command execution error");
+		perror("Process creation error");
 		free(user_input);
 		return (EXIT_FAILURE);
+	}
+	else if (process_id == 0)
+	{
+		args[0] = user_input;
+		args[1] = NULL;
+		if (execve(user_input, args, environ) == -1)
+		{
+			perror("Command execution error");
+			free(user_input);
+			return (EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(process_id, NULL, 0);
 	}
 	free(user_input);
 }
